c6c8f5b3eff93e5eacca9a27924f1746764972b1
citizensyd <59608029+citizensyd@users.noreply.github.com>
2024-09-28 22:03:58 +0200

    Fix - Front env prod

--- Front/src/environments/environment.prod.ts
+++ Front/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  authUrl: 'https://site1.srv546916.hstgr.cloud/api/auth',
-  backendUrl: 'https://site1.srv546916.hstgr.cloud/subscriptions',
-  articleUrl: 'https://site1.srv546916.hstgr.cloud/articles',
-  commentUrl: 'https://site1.srv546916.hstgr.cloud/api/articles',
-  userUrl: 'https://site1.srv546916.hstgr.cloud/api/users',
+  authUrl: 'https://site1.srv546916.hstgr.cloud:3001/api/auth',
+  backendUrl: 'https://site1.srv546916.hstgr.cloud:3001/subscriptions',
+  articleUrl: 'https://site1.srv546916.hstgr.cloud:3001/articles',
+  commentUrl: 'https://site1.srv546916.hstgr.cloud:3001/api/articles',
+  userUrl: 'https://site1.srv546916.hstgr.cloud:3001/api/users',
 };
