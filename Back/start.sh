1a2f03a6f3dd6005d387505c24055e3019c06159
citizensyd <59608029+citizensyd@users.noreply.github.com>
2024-10-08 13:31:59 +0200

    Update - test new deployement after local test

--- Back/start.sh
+++ Back/start.sh
@@ -1,16 +1,32 @@
 #!/bin/sh
 
-# Lire les secrets à partir des fichiers montés par Docker Swarm
-SPRING_DATASOURCE_URL=$(cat /run/secrets/database_url)
-SPRING_DATASOURCE_USERNAME=$(cat /run/secrets/database_username)
-SPRING_DATASOURCE_PASSWORD=$(cat /run/secrets/database_password)
-JWT_SECRET=$(cat /run/secrets/jwt_key)
-CORS_ALLOWED_ORIGINS=$(cat /run/secrets/cors_allowed_origins)
+# Lire les secrets montés par Docker Swarm en production
+if [ "$ENV" = "prod" ]; then
+  SPRING_DATASOURCE_URL=$(cat /run/secrets/database_url)
+  SPRING_DATASOURCE_USERNAME=$(cat /run/secrets/database_username)
+  SPRING_DATASOURCE_PASSWORD=$(cat /run/secrets/database_password)
+  JWT_SECRET=$(cat /run/secrets/jwt_key)
+  CORS_ALLOWED_ORIGINS=$(cat /run/secrets/cors_allowed_origins)
 
-# Lancer l'application Java avec les secrets en tant que propriétés Spring Boot
-exec java -jar app.jar \
-  --spring.datasource.url=$SPRING_DATASOURCE_URL \
-  --spring.datasource.username=$SPRING_DATASOURCE_USERNAME \
-  --spring.datasource.password=$SPRING_DATASOURCE_PASSWORD \
-  --jwt.secret=$JWT_KEY \
-  --cors.allowed-origins=$CORS_ALLOWED_ORIGINS
+  exec java -jar app.jar \
+    --spring.datasource.url=$SPRING_DATASOURCE_URL \
+    --spring.datasource.username=$SPRING_DATASOURCE_USERNAME \
+    --spring.datasource.password=$SPRING_DATASOURCE_PASSWORD \
+    --jwt.secret=$JWT_SECRET \
+    --cors.allowed-origins=$CORS_ALLOWED_ORIGINS
+
+else
+  # En développement local, charger les variables d'environnement depuis dev.env
+#  if [ -f "dev.env" ]; then
+#    echo "Chargement des variables d'environnement depuis dev.env..."
+#    export $(grep -v '^#' dev.env | xargs)
+#  fi
+
+  # Utiliser les variables d'environnement chargées ou définies dans docker-compose.yml
+  exec java -jar app.jar \
+    --spring.datasource.url=$DATABASE_URL \
+    --spring.datasource.username=$DATABASE_USERNAME \
+    --spring.datasource.password=$DATABASE_PASSWORD \
+    --jwt.secret=$JWT_KEY \
+    --cors.allowed-origins=$CORS_ALLOWED_ORIGINS
+fi
--- Dockerfile
+++ Dockerfile
@@ -9,21 +9,24 @@ RUN rm /etc/nginx/conf.d/default.conf
 #    apt-get install -y certbot python3-certbot-nginx && \
 #    rm -rf /var/lib/apt/lists/*
 
-# Copier votre fichier nginx.conf dans le conteneur
+Copier votre fichier nginx.conf dans le conteneur
 COPY nginx.conf /etc/nginx/nginx.conf
 
-# Copier vos fichiers de configuration de site (comme site1.conf)
+#Copier vos fichiers de configuration de site (comme site1.conf)
 COPY site1.conf /etc/nginx/conf.d/site1.conf
 
 # Copier le script wait-for-it.sh
-COPY scripts/wait-for-it.sh /wait-for-it.sh
-RUN chmod +x /wait-for-it.sh
+#COPY scripts/wait-for-it.sh /wait-for-it.sh
+#RUN chmod +x /wait-for-it.sh
 
-# Exposer le port 80
+#Exposer le port 80
 EXPOSE 80
 
+# Exposer le port 443 pour HTTPS
+#EXPOSE 443
+
 # Commande par défaut pour démarrer Nginx, en attendant que le backend et frontend soient disponibles
-CMD /wait-for-it.sh backend:3001 -- /wait-for-it.sh frontend:80 -- nginx -g "daemon off;"
+#CMD /wait-for-it.sh backend:3001 -- /wait-for-it.sh frontend:80 -- nginx -g "daemon off;"
 
 # Ajouter un script pour exécuter certbot et renouveler les certificats automatiquement
 #COPY ./scripts/entrypoint.sh /entrypoint.sh
@@ -31,3 +34,6 @@ CMD /wait-for-it.sh backend:3001 -- /wait-for-it.sh frontend:80 -- nginx -g "dae
 
 # Commande par défaut pour démarrer Nginx et Certbot
 #CMD ["/entrypoint.sh"]
+
+# Commande alternative pour isoler l'utilisation de Certbot et faire fonctionner le backend, le frontend, MySQL et Nginx d'abord
+CMD nginx -g "daemon off;"
